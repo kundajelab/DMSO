d7b63ec705cf427c457f07b8ec7984aabc3553d8
anna shcherbina <annashcherbina@gmail.com>
2017-07-19 13:06:55 -0700

    code to tally the number of peaks/genes falling into each tad region

---
+++ check_tad_enrichment/get_peak_distribution_in_tads.py
@@ -0,0 +1,68 @@
+#calculates the number of peaks from the provided peak set that are in each tad
+import argparse
+import pdb
+
+def parse_args():
+    parser=argparse.ArgumentParser(description="calculates the number of peaks from the provided peakset that are in each tad")
+    parser.add_argument("--tads")
+    parser.add_argument("--peaks")
+    parser.add_argument("--outf")
+    parser.add_argument("--binsize",type=int,default=5000)
+    return parser.parse_args()
+
+def main():
+    args=parse_args()
+    tads=open(args.tads,'r').read().strip().split('\n')
+    peaks=open(args.peaks,'r').read().strip().split('\n')
+    binsize=args.binsize
+    
+    tad_dict=dict()
+    tad_distribution=dict()
+    for tad in tads:
+        tokens=tad.split('\t')
+        chrom='chr'+tokens[0]
+        startval=int(tokens[1])
+        endval=int(tokens[2])
+        if chrom not in tad_dict:
+            tad_dict[chrom]=dict()
+            tad_distribution[chrom]=dict() 
+        tad_dict[chrom][startval]=endval
+        tad_distribution[chrom][startval]=0 
+    print("generated tad dictionary")
+
+    counter=0
+    for peak in peaks:
+        counter+=1
+        if counter % 1000==0:
+            print(str(counter))
+        tokens=peak.split('\t')
+        chrom=tokens[0]
+        if chrom not in tad_dict:
+            continue 
+        startval=int(tokens[1])
+        endval=int(tokens[2])
+        startval=binsize*(startval/binsize)
+        while startval not in tad_dict[chrom]:
+            if startval <=0:
+                break 
+            startval=startval - binsize
+        try:
+            upper_bound = tad_dict[chrom][startval]
+            tad_distribution[chrom][startval]+=1
+        except:
+            continue 
+    print("assigned peaks to tad regions")
+
+
+    outf=open(args.outf,'w')
+    num_empty=0
+    for chrom in tad_distribution:
+        for startval in tad_distribution[chrom]:
+            if tad_distribution[chrom][startval]==0:
+                num_empty+=1
+            else:
+                outf.write(chrom+'\t'+str(startval)+'\t'+str(tad_dict[chrom][startval])+'\t'+str(tad_distribution[chrom][startval])+'\n')
+    outf.write("NumberEmpty\t"+str(num_empty)+'\n')
+    
+if __name__=="__main__":
+    main() 
---
+++ check_tad_enrichment/get_peak_distribution_in_tads.sh
@@ -0,0 +1,6 @@
+#All ATAC-seq peaks 
+python get_peak_distribution_in_tads.py --tads all.tads.bed --peaks ../atacseq_merged.peaks.bed --outf all_atac_peak_tad_distribution.txt
+#Differential ATAC-seq peaks 
+#python get_peak_distribution_in_tads.py --tads all.tads.bed --peaks differential_atac_peaks.bed --outf differential_atac_peak_tad_distribution.txt
+#All expressed genes
+#Differentially expressed genes
