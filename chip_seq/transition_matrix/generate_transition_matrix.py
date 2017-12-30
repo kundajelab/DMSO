953db93a66ba17bc583570306ff53e6398390970
anna shcherbina <annashcherbina@gmail.com>
2017-12-30 14:26:30 -0800

    cluster-specific chip-seq transition matrices

--- chip_seq/transition_matrix/generate_transition_matrix.py
+++ chip_seq/transition_matrix/generate_transition_matrix.py
@@ -43,8 +43,88 @@ def get_diff_dict():
         diff_dict['h3k27me3_down'][tuple(line.split('\t'))]=1
     print('generated dictionaries of differential chipseq peaks')
     return diff_dict 
-    
-def get_transition_matrix(h3k27ac,h3k4me3,h3k27me3,diff_dict,cluster_specific=False,cluster_index=None): 
+
+def get_transition_matrix_cluster(h3k27ac,h3k4me3,h3k27me3,diff_dict,cluster_index):
+    intervals=BedTool("/srv/scratch/annashch/dmso/dp_gp/dpgp_diff_peaks_fold/"+str(cluster_index)+".bed")
+    intersection_h3k27ac=intervals.closest(h3k27ac,wao=True)
+    intersection_h3k4me3=intervals.closest(h3k4me3,wao=True)
+    intersection_h3k27me3=intervals.closest(h3k27me3,wao=True)
+    results=dict() 
+    for i in range(len(intervals)):
+        if i%100==0:
+            print(i) 
+        cur_interval=tuple(intersection_h3k27ac[i][0:3])
+        results[cur_interval]=dict()
+        results[cur_interval]['dmso']=[0,0,0] #h3k27ac, h3k4me3, h3k27me3
+        results[cur_interval]['control']=[0,0,0] 
+        #determine which of 6 possible mark combinations is represented for DMSO & Control
+        if intersection_h3k27ac[i][3].startswith('c'):
+            #is the h3k27ac mark differential or stable?
+            overlap=tuple(intersection_h3k27ac[i][3:6])
+            #is the peak up in dmso?
+            if overlap in diff_dict['h3k27ac_up']:
+                results[cur_interval]['dmso'][0]=1 
+            #is the peak up in control? 
+            elif overlap in diff_dict['h3k27ac_down']:
+                results[cur_interval]['control'][0]=1 
+            else:
+                results[cur_interval]['dmso'][0]=1
+                results[cur_interval]['control'][0]=1
+        if intersection_h3k4me3[i][3].startswith('c'):
+            #is the h3k4me3 mark differential or stable?
+            overlap=tuple(intersection_h3k4me3[i][3:6])
+            #is the peak up in dmso?
+            if overlap in diff_dict['h3k4me3_up']:
+                results[cur_interval]['dmso'][1]=1 
+            #is the peak up in control? 
+            elif overlap in diff_dict['h3k4me3_down']:
+                results[cur_interval]['control'][1]=1 
+            else:
+                results[cur_interval]['dmso'][1]=1
+                results[cur_interval]['control'][1]=1
+
+        if intersection_h3k27me3[i][3].startswith('c'):
+            #is the h3k27me3 mark differential or stable?
+            overlap=tuple(intersection_h3k27me3[i][3:6])
+            #is the peak up in dmso?
+            if overlap in diff_dict['h3k27me3_up']:
+                results[cur_interval]['dmso'][2]=1 
+            #is the peak up in control? 
+            elif overlap in diff_dict['h3k27me3_down']:
+                results[cur_interval]['control'][2]=1 
+            else:
+                results[cur_interval]['dmso'][2]=1
+                results[cur_interval]['control'][2]=1
+    print("completed interval labels")
+    #aggregate results into matrix
+    transition_mat=dict()
+    start_states=set([])
+    end_states=set([]) 
+    for interval in results:
+        start_state=tuple(results[interval]['control'])
+        end_state=tuple(results[interval]['dmso'])
+        start_states.add(start_state)
+        end_states.add(end_state) 
+        if start_state not in transition_mat:
+            transition_mat[start_state]=dict()
+        if end_state not in transition_mat[start_state]:
+            transition_mat[start_state][end_state]=1
+        else:
+            transition_mat[start_state][end_state]+=1
+    outf=open("chipseq_transition_matrix_"+str(cluster_index)+".txt",'w')
+    start_states=list(start_states)
+    end_states=list(end_states)
+    outf.write('\t'+'\t'.join([str(i) for i in end_states])+'\n')
+    for s in start_states:
+        outf.write(str(s))
+        for e in end_states:
+            if e in transition_mat[s]:
+                outf.write('\t'+str(transition_mat[s][e]))
+            else:
+                outf.write('\t0')
+        outf.write('\n')
+
+def get_transition_matrix_global(h3k27ac,h3k4me3,h3k27me3,diff_dict):
     chromsizes=open("hg19.chrom.sizes",'r').read().strip().split('\n')
     window=2000
     results=dict()
@@ -53,10 +133,6 @@ def get_transition_matrix(h3k27ac,h3k4me3,h3k27me3,diff_dict,cluster_specific=Fa
         chrom=tokens[0]
         size=int(tokens[1])
         intervals=BedTool('\n'.join([chrom+'\t'+str(i)+'\t'+str(i+window) for i in range(1,size+2000,2000)]),from_string=True)
-        if cluster_specific==True:
-            cluster_atac=BedTool("/srv/scratch/annashch/dmso/dp_gp/dpgp_diff_peaks_fold/"+str(cluster_index)+".bed")
-            intervals=intervals.intersect(cluster_atac)
-
         print("generated intervals for chromosome:"+str(chrom))
         #get the intersection with each histone mark.
         intersection_h3k27ac=intervals.intersect(h3k27ac,wao=True)
@@ -148,6 +224,6 @@ def get_transition_matrix(h3k27ac,h3k4me3,h3k27me3,diff_dict,cluster_specific=Fa
 
 h3k27ac,h3k4me3,h3k27me3=get_sources()
 diff_dict=get_diff_dict()
-get_transition_matrix(h3k27ac,h3k4me3,h3k27me3,diff_dict) 
+#get_transition_matrix_global(h3k27ac,h3k4me3,h3k27me3,diff_dict) 
 for cluster_index in range(1,7):
-    get_transition_matrix(h3k27ac,h3k4me3,h3k27me3,diff_dict,cluster_specific=True,cluster_index=cluster_index) 
+    get_transition_matrix_cluster(h3k27ac,h3k4me3,h3k27me3,diff_dict,cluster_index=cluster_index) 
